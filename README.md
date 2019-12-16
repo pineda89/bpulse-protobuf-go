30f98b40b15e985da8346a5095afcfeec724090d
pineda89 <dani_pineda89@hotmail.com>
2019-12-16 10:38:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bpulse-protobuf-go
+Go Pulse proto messages RQ/RS structs for BPulse
---
+++ bpulse-protobuf-go.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    PROTO "github.com/pineda89/bpulse-protobuf-go/com.bpulse.protobuf/go"
+
+)
+
+func main(){
+    _ = PROTO.PulsesRQ{}
+    _ = PROTO.PulsesRS{}
+}
